58db58f08c0c933574b6daf5da12808539dbb2e2
William Kruse <williamkruse4@gmail.com>
2022-04-20 12:13:19 +0200

    har lavet 2 klasses og de kan buildes

---
+++ miniprojekt2022/Shared/Models/BookingItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace miniprojekt2022.Shared.Models
+{
+	public class BookingItem
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string? ObjectIdBooking { get; set; }
+
+		[Required]
+		[BsonElement("kunde_navn")]
+		public string KundeNavn { get; set; }
+
+		[Required]
+		[BsonElement("email")]
+		public string Email { get; set; }
+
+		[Required]
+		[BsonElement("telefon")]
+		public string Telefon { get; set; }
+
+		[Required]
+		[BsonElement("shelter_id")]
+		public string ShelterId { get; set; }
+
+
+		[BsonElement("start_dato")]
+		public DateTime? StartDato { get; set; }
+
+
+		[BsonElement("slut_dato")]
+		public DateTime? SlutDato { get; set; }
+
+		public BookingItem(string kundeNavn = "", string email = "", string telefon = "", string shelterId = "", DateTime startDato = new DateTime(), DateTime slutDato = new DateTime())
+		{
+			this.KundeNavn = kundeNavn;
+			this.Email = email;
+			this.Telefon = telefon;
+			this.ShelterId = shelterId;
+			this.StartDato = startDato;
+			this.SlutDato = slutDato;
+		}
+	}
+}
+
---
+++ miniprojekt2022/Shared/Models/ShelterItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace miniprojekt2022.Shared.Models
+{
+	public class ShelterItem
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string ObjectIdShelter { get; set; }
+
+		[Required]
+		[BsonElement("navn")]
+		public string ShelterNavn { get; set; }
+
+		
+		[BsonElement("lang_beskr")]
+		public string? Beskrivelse { get; set; }
+	
+		[Required]
+		[BsonElement("antal_pl")]
+		public int AntalPersoner { get; set; }
+
+
+		public ShelterItem(string shelterNavn ="", string? beskrivelse = "", int antalPersoner = -1)
+		{
+			this.ShelterNavn = shelterNavn;
+			this.Beskrivelse = beskrivelse;
+			this.AntalPersoner = antalPersoner;
+		}
+	}
+}
+
